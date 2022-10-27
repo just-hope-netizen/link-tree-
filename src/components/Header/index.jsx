a8eeda0696383cb241a807cb3e394515ca7f84a0
just-hope-netizen <hopepeter007@gmail.com>
2022-10-27 23:22:56 +0100

    Refactor

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -5,8 +5,9 @@ export const Header =()=>{
 return (
   <header className='d-flex flex-column align-items-center position-relative'>
     <div className='img-wrapper'>
-      <img src={img} alt='owner' className='img' id='profile__img' />
-      <p className='img-name'>Annette Black</p>
+      <img src={img} alt='profile ' className='img' id='profile__img' />
+      <p className='img-name' id='twitter'>@hope_imevbore</p>
+      <p className='d-none' id='slack'>hope peter</p>
       <img src={shareBtn} alt='share icon' className='share-btn d-none d-md-block' />
       <ShareIcon className='share-btn d-md-none' />
     </div>
